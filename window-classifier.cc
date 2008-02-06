0ec4f3134c1ba5b443626ce158d69a23d4fe3fd0
derat <derat@3dc29a13-9b0d-0410-8e9a-90d08853e164>
2008-02-06 22:27:52 +0000

    move WindowProperties out of the Window class
    git-svn-id: file:///home/derat/svn/wham@151 3dc29a13-9b0d-0410-8e9a-90d08853e164

--- window-classifier.cc
+++ window-classifier.cc
@@ -3,36 +3,11 @@
 
 #include "window-classifier.h"
 
-#include <sstream>
+#include "window.h"
 
 namespace wham {
 
 ref_ptr<WindowClassifier> WindowClassifier::singleton_(new WindowClassifier);
-
-
-string WindowProperties::DebugString() const {
-  ostringstream out;
-  out << "window_name=\"" << window_name << "\"\n"
-      << "icon_name=\"" << icon_name << "\"\n"
-      << "command=\"" << command << "\"\n"
-      << "app_name=\"" << app_name << "\"\n"
-      << "app_class=\"" << app_class << "\"\n"
-      << "x=" << x << "\n"
-      << "y=" << y << "\n"
-      << "width=" << width << "\n"
-      << "height=" << height << "\n"
-      << "min_width=" << min_width << "\n"
-      << "min_height=" << min_height << "\n"
-      << "max_width=" << max_width << "\n"
-      << "max_height=" << max_height << "\n"
-      << "width_inc=" << width_inc << "\n"
-      << "height_inc=" << height_inc << "\n"
-      << "min_aspect=" << min_aspect << "\n"
-      << "max_aspect=" << max_aspect << "\n"
-      << "base_width=" << base_width << "\n"
-      << "base_height=" << base_height << "\n";
-  return out.str();
-}
 
 
 void WindowConfig::Merge(const WindowConfig& config) {
