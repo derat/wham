3c4be67657e959bf8923c285efa8cd4cb70b1edb
Daniel Erat <dan@erat.org>
2009-01-02 15:11:54 -0800

    handle keyboard mapping events; disable test timer

--- main.cc
+++ main.cc
@@ -6,19 +6,11 @@
 
 using namespace wham;
 
-struct TestTimeoutFunction : public XServer::TimeoutFunction {
-  void operator()() {
-    DEBUG << "timeout called";
-    XServer::Get()->RegisterTimeout(new TestTimeoutFunction, 1);
-  }
-};
-
 int main(int argc, char** argv) {
   CHECK(XServer::Get()->Init());
   WindowManager window_manager;
   window_manager.SetupDefaultCrap();
   CHECK(window_manager.LoadConfig("config"));
-  XServer::Get()->RegisterTimeout(new TestTimeoutFunction, 1);
   XServer::Get()->RunEventLoop(&window_manager);
   return 0;
 }
